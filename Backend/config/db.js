789584de6af3571bdc0e3e7e307b88656b69ba63
mushthaque <musthaquemt7@gmail.com>
2024-12-22 23:15:00 +0530

    updated DB connection

--- Backend/config/db.js
+++ Backend/config/db.js
@@ -3,5 +3,5 @@
 import mongoose from 'mongoose'
 
 export const connectDB = async () =>{
-    await mongoose.connect('mongodb+srv://musthaquemt7:7SucFY3byUk09JX1@cluster0.9z0ug.mongodb.net/grocery').then(()=>{console.log("DB connected")});
+    await mongoose.connect(`mongodb+srv://musthaquemt7:${process.env.MONGO_PASSWORD}@cluster0.9z0ug.mongodb.net/grocery`).then(()=>{console.log("DB connected")});
 }
